fd889ceff0c550aab1c61d7b361c6e32c73eaf56
yKaayo <caiokaea@gmail.com>
2024-10-04 19:38:25 -0300

    Preparing the basic
    Installed Tailwind and doing others things

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      black: "#000",
+      white: "#fff",
+    },
+    fontFamily: {
+      display:
+        'UberMoveText, system-ui, "Helvetica Neue", Helvetica, Arial, sans-serif',
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
   },
   plugins: [],
-}
-
+};
